56dfd3f651a16a108cb6fc3a793ac78fd71c94fc
stuartbowe <stuart@spotlightadata.co.uk>
2019-01-24 11:17:16 +0000

    updated readmes

--- setup.py
+++ setup.py
@@ -10,14 +10,25 @@ Created on Wed Jan 23 11:18:21 2019
 
 from setuptools import setup
 
+
+VERSION = None
+with open("./VERSION") as f:
+    VERSION = f.read()
+
+
 setup(
     name='nanowire_flask',
-    version='0.1dev',
     description='Tool for creating nanowire tools with the flask structure.',
+    version=VERSION,
+    keywords=['flask', 'API', 'nanowire', 'spotlight data'],
     url = 'https://github.com/SpotlightData/nanowire_flask',
     author='Stuart Bowe',
     author_email='stuart@spotlightdata.co.uk',
     packages=['nanowire_flask'],
     license='MIT',
-    long_description=open('README.txt').read(),
+    long_description=open('README.md').read(),
+    long_destription_content_type='text/markdown',
+    data_files=[
+        ('.', ['VERSION'])
+    ]
 )
