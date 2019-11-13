6a4ec51fb4b7d739941b6110e83e3e11801b2e9a
Augustinas <augustinas@spotlightdata.co.uk>
2019-11-13 19:51:36 +0000

    (feat) use env port

--- tests/image_server.py
+++ tests/image_server.py
@@ -6,10 +6,11 @@ Created on Tue May 21 12:00:51 2019
 @author: stuart
 """
 
-#run an example image server
+# run an example image server
 
 
 import numpy as np
+import os
 from pyzbar.pyzbar import decode
 
 import nanowire_flask as nf
@@ -18,17 +19,18 @@ from nanowire_flask.image_tools import mount_Image_function
 
 
 def test_image_function(img, variables):
-    
+
     print("RECEVED IMAGE REQUEST")
 
     result = decode(img)
-    
-    out = {"text":result[0].data.decode(),
-           "variables":variables, 
-           "shape":list(np.shape(img))}
-    
+
+    out = {"text": result[0].data.decode(),
+           "variables": variables,
+           "shape": list(np.shape(img))}
+
     return out
+
 
 print("FOUND VERSION", nf.__version__)
 
-mount_Image_function(test_image_function, port=5001)
+mount_Image_function(test_image_function, port=int(os.environ['PORT']))
