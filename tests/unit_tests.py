1f976112aa81a5fd1ae71aca7b535f51445c6287
Augustinas <augustinas@spotlightdata.co.uk>
2019-11-13 19:27:55 +0000

    (feat) add text unit cases

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -103,5 +103,69 @@ class csv_server_test_case(ServerTest):
         self.assertTrue(out['shape'] == [2, 3])
 
 
+class text_server_test_case(ServerTest):
+    def setUp(self):
+        self.start_server('text_server.py')
+
+    def test_text_json_example(self):
+
+        d = json.dumps({"text": "Example of text.",
+                        "clean_text": 0,
+                        "deactivate_entities": 0,
+                        "deactivate_summary": 0})
+
+        heads = {"Content-Type": "application/json"}
+
+        r = requests.post(self.url, headers=heads, data=d)
+
+        out = r.json()
+
+        self.assertTrue('text' in out.keys())
+        self.assertTrue('variables' in out.keys())
+        self.assertTrue(out['text'] == "Example of text.")
+
+    def test_text_file_example(self):
+
+        text_target = 'http://0.0.0.0:5000/model/predict'
+        f = open(local_file('test_text2.txt'), 'rb')
+        files = {'doc': ('test_text2.txt', f)}
+        r = requests.post(text_target + '?threshold=0.1', files=files)
+        f.close()
+        out = r.json()
+
+        self.assertTrue('text' in out.keys())
+
+        self.assertTrue('variables' in out.keys())
+
+        self.assertTrue('threshold' in out['variables'].keys())
+
+        with open(local_file('test_text2.txt'), "r") as f:
+            t = f.read()
+            f.close()
+
+        self.assertTrue(out['text'] == t)
+
+    def test_text_from_server(self):
+
+        d = json.dumps({"contentUrl": self.file_url('test_text3.txt'),
+                        "clean_text": 0})
+
+        heads = {"Content-Type": "application/json"}
+
+        r = requests.post(self.url, headers=heads, data=d)
+
+        out = r.json()
+        print(out)
+
+        self.assertTrue('text' in out.keys())
+        self.assertTrue('variables' in out.keys())
+
+        with open(local_file('test_text3.txt'), "r") as f:
+            t = f.read()
+            f.close()
+
+        self.assertTrue(out['text'] == t)
+
+
 tracemalloc.start()
 unittest.main()
